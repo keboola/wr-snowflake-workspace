b8423a3bc0804ad798d06826f23d85c91f6003f9
Ondra Jodas <ondrej.jodas@keboola.com>
2020-06-30 14:40:07 +0200

    test connection sync action

--- src/Config.php
+++ src/Config.php
@@ -2,15 +2,31 @@
 
 declare(strict_types=1);
 
-namespace MyComponent;
+namespace Keboola\DbWriter\SnowflakeWorkspace;
 
 use Keboola\Component\Config\BaseConfig;
+use Keboola\Component\UserException;
 
 class Config extends BaseConfig
 {
-    // @todo implement your custom getters
-    public function getFoo(): string
+    public function getStorageApiUrl(): string
     {
-        return $this->getValue(['parameters', 'foo']);
+        if (!getenv('KBC_URL')) {
+            throw new UserException('Missing KBC API url');
+        }
+        return getenv('KBC_URL');
+    }
+
+    public function getStorageApiToken(): string
+    {
+        if (!getenv('KBC_TOKEN')) {
+            throw new UserException('Missing KBC API token');
+        }
+        return getenv('KBC_TOKEN');
+    }
+
+    public function getWorkspaceId(): string
+    {
+        return $this->getValue(['parameters', 'workspaceId']);
     }
 }
---
+++ src/Writer/Snowflake.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\DbWriter\SnowflakeWorkspace\Writer;
+
+use Keboola\Component\UserException;
+use Keboola\DbWriter\SnowflakeWorkspace\Config;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\ClientException;
+use Keboola\StorageApi\Workspaces;
+
+class Snowflake
+{
+    private Client $client;
+
+    private Config $config;
+
+    public function __construct(Client $client, Config $config)
+    {
+        $this->config = $config;
+        $this->client = $client;
+    }
+
+    public function testConnectionAction(Config $config): array
+    {
+        $workspaces = new Workspaces($this->client);
+
+        try {
+            $workspaces->getWorkspace($config->getWorkspaceId());
+        } catch (ClientException $clientException) {
+            throw new UserException($clientException->getMessage(), 0, $clientException);
+        }
+
+        return [
+            'status' => 'success'
+        ];
+    }
+
+}
